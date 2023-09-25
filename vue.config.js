5793ea8b110b4466e05d7481721a961869d7e899
qi.wu <qi.wu@70mai.com>
2023-09-25 14:00:24 +0800

    Upgrade electron version

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions:{
+    electronBuilder:{
+      nodeIntegration:true,
+      builderOptions: {
+        asar:false
+      }
+    }
+  },
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
+    target:"electron-renderer",
+
+  
+  }
+
+
 })
